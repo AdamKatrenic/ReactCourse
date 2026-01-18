3f02808b373dac78571b893b798225c1dbf2f9d0
AdamKatrenic <adamkatrenic@gmail.com>
2026-01-18 18:05:30 +0100

    pridavam testy appky

---
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -0,0 +1,73 @@
+import { it, expect, describe, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import axios from 'axios';
+import { HomePage } from './HomePage';
+
+vi.mock('axios');
+
+const mockedAxios = vi.mocked(axios, true);
+
+describe('HomePage Component', () => {
+  let loadCart;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    loadCart = vi.fn();
+
+    mockedAxios.get.mockImplementation(async (urlPath) => {
+      if (urlPath === '/api/products') {
+        return {
+          data: [
+            {
+              id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+              image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+              name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+              rating: {
+                stars: 4.5,
+                count: 87
+              },
+              priceCents: 1090,
+              keywords: ['socks', 'sports', 'apparel']
+            },
+            {
+              id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+              image: 'images/products/intermediate-composite-basketball.jpg',
+              name: 'Intermediate Size Basketball',
+              rating: {
+                stars: 4,
+                count: 127
+              },
+              priceCents: 2095,
+              keywords: ['sports', 'basketballs']
+            }
+          ]
+        };
+      }
+
+      return { data: [] };
+    });
+  });
+
+  it('displays the products correctly', async () => {
+  render(
+    <MemoryRouter>
+      <HomePage cart={[]} loadCart={loadCart} />
+    </MemoryRouter>
+  );
+
+  // počkaj, kým sa produkty zobrazia podľa textu
+  expect(await screen.findByText('Black and Gray Athletic Cotton Socks - 6 Pairs'))
+    .toBeInTheDocument();
+
+  expect(await screen.findByText('Intermediate Size Basketball'))
+    .toBeInTheDocument();
+
+  // over, že axios sa zavolal
+  expect(mockedAxios.get).toHaveBeenCalledWith('/api/products');
+});
+
+
+
+});
